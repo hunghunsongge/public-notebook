b4f64bc4c3f4bfa3a7e6196f9e7e38de2c129721
hunghunsongge <1127588635@qq.com>
2020-12-01 18:35:44 +0800

    2020年12月1日

--- program-py/py/2.py
+++ program-py/py/2.py
@@ -1,1 +1,13 @@
 import jieba
+dk = {}
+with open('D:\programme\program-py\py\data.txt','r') as f:
+    s=f.readlines()
+for i in s:
+    k=jieba.lcut(s,cut_all = True)
+    for j in k:
+        if len(j)==2:
+            dk[j]=dk.get(j,0)+1
+dp = list(dk.items())
+dp.sort(key= lambda x:int(x[1]), reverse = True)
+for i in range(10):
+    print('{}:{}'.format(dp[i][0],dp[i][1]))
